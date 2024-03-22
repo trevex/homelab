9efc9c37d0215a5ff982ea06354c38f5ac76e588
Niklas Voss <niklas.voss@gmail.com>
2024-03-22 22:09:30 +0100

    Refactored, but complete cmdAdd implementation (untested)

--- vxlan-cni/go.mod
+++ vxlan-cni/go.mod
@@ -6,14 +6,16 @@ toolchain go1.22.1
 
 require (
 	github.com/containernetworking/cni v1.1.2
-	github.com/containernetworking/plugins v1.4.0
+	github.com/containernetworking/plugins v1.4.1
 	github.com/vishvananda/netlink v1.2.1-beta.2
 	golang.org/x/sys v0.18.0
 )
 
 require (
+	github.com/coreos/go-iptables v0.7.0 // indirect
 	github.com/cybozu-go/netutil v1.4.7 // indirect
 	github.com/onsi/gomega v1.31.1 // indirect
+	github.com/safchain/ethtool v0.3.0 // indirect
 	github.com/vishvananda/netns v0.0.4 // indirect
 	golang.org/x/net v0.22.0 // indirect
 )
--- vxlan-cni/main.go
+++ vxlan-cni/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -9,7 +10,11 @@ import (
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
+	current "github.com/containernetworking/cni/pkg/types/100"
 	"github.com/containernetworking/cni/pkg/version"
+	"github.com/containernetworking/plugins/pkg/ip"
+	"github.com/containernetworking/plugins/pkg/ipam"
+	"github.com/containernetworking/plugins/pkg/ns"
 	bv "github.com/containernetworking/plugins/pkg/utils/buildversion"
 	"github.com/containernetworking/plugins/pkg/utils/sysctl"
 	"github.com/cybozu-go/netutil"
@@ -117,7 +122,146 @@ func getDefaultLinkIP() (net.IP, error) {
 	return addrs[0].IP, nil // TODO: safe?
 }
 
+func ensureBridge(n *NetConf, brName string) (*netlink.Bridge, error) {
+	err := netlink.LinkAdd(&netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:   brName,
+			MTU:    n.MTU,
+			TxQLen: -1,
+		},
+	})
+
+	// We ignore error if it the bridge already existed
+	if err != nil && err != syscall.EEXIST {
+		return nil, fmt.Errorf("could not add %q: %v", brName, err)
+	}
+
+	// Fetch the bridge
+	br, err := bridgeByName(brName)
+	if err != nil {
+		return nil, err
+	}
+
+	// We want to own the routes for this interface
+	_, _ = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/accept_ra", brName), "0")
+	// We disable Spanning Tree Protocol
+	if err := os.WriteFile(fmt.Sprintf("/sys/class/net/%s/bridge/stp_state", brName), []byte("0"), 0644); err != nil {
+		return nil, err
+	}
+
+	// And make sure it is up
+	if err := netlink.LinkSetUp(br); err != nil {
+		return nil, err
+	}
+
+	return br, nil
+}
+
+func ensureVXLAN(n *NetConf, vxName string, br *netlink.Bridge) (netlink.Link, error) {
+	nodeIP, err := getDefaultLinkIP()
+	if err != nil {
+		return nil, err
+	}
+
+	err = netlink.LinkAdd(&netlink.Vxlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name: vxName,
+		},
+		VxlanId:  n.VNI,
+		Port:     n.Port,
+		Learning: false,
+		SrcAddr:  nodeIP,
+	})
+
+	// We ignore error if it the bridge already existed
+	if err != nil && err != syscall.EEXIST {
+		return nil, fmt.Errorf("could not add %q: %v", vxName, err)
+	}
+
+	// Fetch the link
+	vx, err := netlink.LinkByName(vxName)
+	if err := netlink.LinkSetMaster(vx, br); err != nil {
+		return nil, fmt.Errorf("failed to connect %q to bridge %v: %v", vx.Attrs().Name, br.Attrs().Name, err)
+	}
+
+	// And make sure it is up
+	if err := netlink.LinkSetUp(vx); err != nil {
+		return nil, err
+	}
+
+	return vx, nil
+}
+
+func createVeth(n *NetConf, ifName string, netns ns.NetNS, br *netlink.Bridge) (string, string, error) {
+	hostIfName := ""
+	contIfName := ""
+
+	err := netns.Do(func(hostNS ns.NetNS) error {
+		hostVeth, contVeth, err := ip.SetupVeth(ifName, n.MTU, "", hostNS)
+		hostIfName = hostVeth.Name
+		contIfName = contVeth.Name
+		return err
+	})
+	if err != nil {
+		return hostIfName, contIfName, err
+	}
+
+	// Need to lookup hostVeth again as its index has changed during ns move
+	hostVeth, err := netlink.LinkByName(ifName)
+	if err != nil {
+		return hostIfName, contIfName, fmt.Errorf("failed to lookup %q: %v", ifName, err)
+	}
+
+	// connect host veth end to the bridge
+	if err := netlink.LinkSetMaster(hostVeth, br); err != nil {
+		return hostIfName, contIfName, fmt.Errorf("failed to connect %q to bridge %v: %v", hostVeth.Attrs().Name, br.Attrs().Name, err)
+	}
+
+	return hostIfName, contIfName, nil
+}
+
+func resultForInterfaces(netns ns.NetNS, br *netlink.Bridge, hostIfName, contIfName string) (*current.Result, error) {
+	hostIf := &current.Interface{
+		Name: hostIfName,
+	}
+	hostVeth, err := netlink.LinkByName(hostIf.Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to lookup %q: %v", hostIf.Name, err)
+	}
+	hostIf.Mac = hostVeth.Attrs().HardwareAddr.String()
+
+	contIf := &current.Interface{
+		Name: contIfName,
+	}
+	err = netns.Do(func(hostNS ns.NetNS) error {
+		contVeth, err := netlink.LinkByName(contIfName)
+		if err != nil {
+			return fmt.Errorf("failed to lookup %q: %v", contIfName, err)
+		}
+		contIf.Mac = contVeth.Attrs().HardwareAddr.String()
+		contIf.Sandbox = netns.Path()
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &current.Result{
+		CNIVersion: current.ImplementedSpecVersion,
+		Interfaces: []*current.Interface{
+			&current.Interface{
+				Name: br.Attrs().Name,
+				Mac:  br.Attrs().HardwareAddr.String(),
+			},
+			hostIf,
+			contIf,
+		},
+	}, nil
+}
+
 func cmdAdd(args *skel.CmdArgs) error {
+	success := false
+
 	n, err := loadNetConf(args.StdinData)
 	if err != nil {
 		return err
@@ -133,82 +277,93 @@ func cmdAdd(args *skel.CmdArgs) error {
 
 	// Let's create the bridge
 	brName := fmt.Sprintf("bridge%d", n.VNI)
-	err = netlink.LinkAdd(&netlink.Bridge{
-		LinkAttrs: netlink.LinkAttrs{
-			Name:   brName,
-			MTU:    n.MTU,
-			TxQLen: -1,
-		},
-	})
-	// We ignore error if it the bridge already existed
-	if err != nil && err != syscall.EEXIST {
-		return fmt.Errorf("could not add %q: %v", brName, err)
-	}
-	// Fetch the bridge
-	br, err := bridgeByName(brName)
-	if err != nil {
-		return err
-	}
-	// We want to own the routes for this interface
-	_, _ = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/accept_ra", brName), "0")
-	// We disable Spanning Tree Protocol
-	if err := os.WriteFile(fmt.Sprintf("/sys/class/net/%s/bridge/stp_state", brName), []byte("0"), 0644); err != nil {
-		return err
-	}
-	// And make sure it is up
-	if err := netlink.LinkSetUp(br); err != nil {
-		return err
-	}
-
-	// For the VXLAN interface we'll need the node IP
-	nodeIP, err := getDefaultLinkIP()
+	br, err := ensureBridge(n, brName)
 	if err != nil {
 		return err
 	}
 
 	// Let's make sure the vxlan interface exists (otherwise we create it)
 	vxName := fmt.Sprintf("vxlan%d", n.VNI)
-	err = netlink.LinkAdd(&netlink.Vxlan{
-		LinkAttrs: netlink.LinkAttrs{
-			Name: vxName,
-		},
-		VxlanId:  n.VNI,
-		Port:     n.Port,
-		Learning: false,
-		SrcAddr:  nodeIP,
-	})
-	// We ignore error if it the bridge already existed
-	if err != nil && err != syscall.EEXIST {
-		return fmt.Errorf("could not add %q: %v", vxName, err)
+	_, err = ensureVXLAN(n, vxName, br)
+
+	netns, err := ns.GetNS(args.Netns)
+	if err != nil {
+		return fmt.Errorf("failed to open netns %q: %v", args.Netns, err)
 	}
-	// Fetch the link
-	vx, err := netlink.LinkByName(vxName)
-	if err := netlink.LinkSetMaster(vx, br); err != nil {
-		return fmt.Errorf("failed to connect %q to bridge %v: %v", vx.Attrs().Name, br.Attrs().Name, err)
-	}
-	// And make sure it is up
-	if err := netlink.LinkSetUp(vx); err != nil {
+	defer netns.Close()
+
+	// Let's create the container and host interface
+	ifName := args.IfName
+	hostIfName, contIfName, err := createVeth(n, ifName, netns, br)
+
+	// Start preparing the result with our interfaces
+	result, err := resultForInterfaces(netns, br, hostIfName, contIfName)
+	if err != nil {
 		return err
 	}
 
 	// Let's retrieve the IP and Prefix from IPAM
-	// TODO
+	if n.IPAM.Type != "" {
+		r, err := ipam.ExecAdd(n.IPAM.Type, args.StdinData)
+		if err != nil {
+			return err
+		}
 
-	// Let's create the container and host interface
-	// TODO
+		// Release IP in case of failure
+		defer func() {
+			if !success {
+				ipam.ExecDel(n.IPAM.Type, args.StdinData)
+			}
+		}()
 
-	// Next we assign the IP to the container interface
+		// Convert whatever the IPAM result was into the current Result type
+		ipamResult, err := current.NewResultFromResult(r)
+		if err != nil {
+			return err
+		}
 
-	// Up!
+		result.IPs = ipamResult.IPs
+		result.Routes = ipamResult.Routes
+		result.DNS = ipamResult.DNS
 
+		if len(result.IPs) == 0 {
+			return errors.New("IPAM plugin returned missing IP config")
+		}
+
+		// Configure the container hardware address and IP address(es)
+		if err := netns.Do(func(_ ns.NetNS) error {
+			_, _ = sysctl.Sysctl(fmt.Sprintf("net/ipv6/conf/%s/accept_dad", args.IfName), "0")
+			_, _ = sysctl.Sysctl(fmt.Sprintf("net/ipv4/conf/%s/arp_notify", args.IfName), "1")
+			return ipam.ConfigureIface(args.IfName, result)
+		}); err != nil {
+			return err
+		}
+	} else { // ipam.Configure will set link to up, if ipam not used, we have to manually do it
+		if err := netns.Do(func(_ ns.NetNS) error {
+			link, err := netlink.LinkByName(args.IfName)
+			if err != nil {
+				return fmt.Errorf("failed to retrieve link: %v", err)
+			}
+
+			if err = netlink.LinkSetUp(link); err != nil {
+				return fmt.Errorf("failed to set %q up: %v", args.IfName, err)
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+	}
+
+	success = true
+
+	return types.PrintResult(result, n.CNIVersion)
+}
+
+func cmdDel(args *skel.CmdArgs) error {
 	return nil
 }
 
 func cmdCheck(args *skel.CmdArgs) error {
-	return nil
-}
-
-func cmdDel(args *skel.CmdArgs) error {
 	return nil
 }
 
